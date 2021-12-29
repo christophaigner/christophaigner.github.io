8a4da41f3d7ede967868c523d410c4475a5d9acf
Christoph Aigner <25478494+christophaigner@users.noreply.github.com>
2021-12-29 20:41:24 +0100

    Add text-to-speech

---
+++ assets/js/text_to_speech.js
@@ -0,0 +1,52 @@
+// @ts-check
+
+function speakArticle() {
+  const postContent = document.getElementById("blog_post");
+  const readableText = postContent.innerText;
+  speak(readableText);
+}
+
+function updateIcon(playing) {
+  const speakArticleButton = document.getElementById("speakArticleButton");
+  const state = playing ? "pause" : "play";
+  speakArticleButton.innerHTML = `<i class="fas fa-regular fa-${state}"></i>`;
+}
+
+function speak(message) {
+  if (window.speechSynthesis.speaking && window.speechSynthesis.paused) {
+    window.speechSynthesis.resume();
+    return;
+  } else if (window.speechSynthesis.speaking) {
+    window.speechSynthesis.pause();
+    return;
+  }
+
+  const utterance = new SpeechSynthesisUtterance();
+  const voices = window.speechSynthesis.getVoices();
+  utterance.voice = voices.find(voice => voice.voiceURI === "com.apple.speech.synthesis.voice.samantha");
+  utterance.volume = 1;
+  utterance.rate = 8;
+  utterance.pitch = 0.8;
+  utterance.lang = "en-US";
+  window.utterance = utterance;
+
+  const maxLength = 32_767;
+
+  if (message.length >= maxLength) {
+    utterance.text = message.substring(0, maxLength);
+    utterance.onend = function (_) {
+        speak(message.substring(maxLength, message.length));
+    };
+  } else {
+    utterance.text = message;
+    utterance.onend = function (_) {
+      window.utterance = undefined;
+    };
+  }
+
+  utterance.onpause = () => updateIcon(false);
+  utterance.onresume = () => updateIcon(true);
+  utterance.onstart = () => updateIcon(true);
+
+  speechSynthesis.speak(utterance);
+}
